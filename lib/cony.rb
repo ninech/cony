5f3c9206a5a3bf98c350e6baa9584647adad2264
Raffael Schmid <raf@nine.ch>
2014-04-17 11:09:14 +0200

    handle activesupport 3 and 4

--- lib/cony.rb
+++ lib/cony.rb
@@ -1,4 +1,10 @@
-require 'active_support/core_ext/object/deep_dup'
+begin
+  # activesupport 4
+  require 'active_support/core_ext/object/deep_dup'
+rescue LoadError
+  # activesupport 3
+  require 'active_support/core_ext/hash/deep_dup'
+end
 require 'active_support/configurable'
 
 require 'cony/active_record'
